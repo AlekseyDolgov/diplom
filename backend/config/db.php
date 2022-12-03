1eff6217ef507bbeeb7133b2f1a0c936a684602e
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2022-12-03 18:37:45 +0300

    Подключение к БД

---
+++ backend/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=diplom',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
+
---
+++ frontend/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=diplom',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
